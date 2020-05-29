103633651869d91834049b359e12308b82a9b2c0
Adel Amr <os@oss-macbook-pro.localdomain>
2020-05-29 04:51:02 +0200

    Create and configure logic for  interaction service

---
+++ src/app/shared/services/interaction/interaction.service.ts
@@ -0,0 +1,26 @@
+import { Injectable } from "@angular/core";
+import { BehaviorSubject } from "rxjs";
+
+@Injectable({
+  providedIn: "root"
+})
+export class InteractionService {
+  isProductAdded: BehaviorSubject<boolean> = new BehaviorSubject<boolean>(
+    false
+  );
+  isShoppingCartClicked: BehaviorSubject<boolean> = new BehaviorSubject<
+    boolean
+  >(false);
+  isHeaderClicked: BehaviorSubject<boolean> = new BehaviorSubject<boolean>(
+    false
+  );
+
+  constructor() {}
+
+  confirmHeaderClicked = (): void => this.isHeaderClicked.next(true);
+
+  toggleShoppingCartModal = (): void =>
+    this.isShoppingCartClicked.next(!this.isShoppingCartClicked.value);
+
+  confirmProductAddition = (): void => this.isProductAdded.next(true);
+}
