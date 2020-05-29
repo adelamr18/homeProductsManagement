a32d61d425ed9fedfe573610c35685d0fd982a01
Adel Amr <os@oss-macbook-pro.localdomain>
2020-05-29 04:43:33 +0200

    Create defines file to contain static texts and routes

---
+++ src/app/shared/constants/defines.ts
@@ -0,0 +1,66 @@
+export const titles = {
+  header: "Products Management",
+  tableId: "Id",
+  productName: "Product Name",
+  price: "Price",
+  quantity: "Quantity",
+  products: "Products",
+  shipping: "Shipping",
+  payment: "Payment",
+  overview: "Overview",
+  success: "Success",
+  basket: "Basket",
+  addProducts: "Add some products to your home",
+  order: "Order",
+  total: "Total:",
+  continue: "Continue",
+  continueShopping: "Continue shopping",
+  submit: "Submit"
+};
+
+export const errors = {
+  alreadyAdded: "You have already added this product",
+  firstNameRequired: " Firstname is required",
+  firstNameLetters: "Firstname must be letters only",
+  lastNameRequired: "Lastname is required",
+  lastNameLetters: "Lastname must be letters only",
+  addressRequired: "Address is required",
+  mobileRequired: " Mobile is required",
+  mobileMinLength: " Mobile must be at least 13 digits",
+  mobileWrongPattern: "Mobile number must have a plus sign and numbers only",
+  cardHolderRequired: "Cardholder is required",
+  cardHolderLetters: "Cardholder must be letters only",
+  ibanRequired: "Iban is required",
+  ibanInvalid: "IBAN is invalid"
+};
+
+export const placeholders = {
+  search: "Search for a product"
+};
+
+export const formsInfo = {
+  shipping: "SHIPPING",
+  firstName: "Firstname",
+  lastName: "Lastname",
+  address: "Address",
+  mobile: "Mobile",
+  payment: "PAYMENT",
+  cardHolder: "Cardholder",
+  iban: "Iban",
+  overview: "OVERVIEW",
+  success: "SUCCESS",
+  purchaseSuccess: "Thank you for your purchase"
+};
+
+export const validators = {
+  capitalAndSmall: "^[A-Za-z]+$",
+  mobileFormat: "^(\\+)?(\\d+)$"
+};
+
+export const routes = {
+  products: "products",
+  shipping: "shipping",
+  payment: "payment",
+  overview: "overview",
+  success: "success"
+};
