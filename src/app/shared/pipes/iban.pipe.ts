b7614812c0852acceab45a31c39420325672bc60
Adel Amr <os@oss-macbook-pro.localdomain>
2020-05-29 05:08:55 +0200

    Add iban pipe to format users iban

---
+++ src/app/shared/pipes/iban.pipe.ts
@@ -0,0 +1,14 @@
+import { Pipe, PipeTransform } from "@angular/core";
+
+@Pipe({
+  name: "iban"
+})
+export class IbanPipe implements PipeTransform {
+  transform(value: string): string {
+    // remove existing spaces
+    let lIban: string = value.replace(" ", "");
+    // place a space after every 4th character
+    lIban = lIban.replace(/(.{4})/g, "$1 ");
+    return lIban;
+  }
+}
